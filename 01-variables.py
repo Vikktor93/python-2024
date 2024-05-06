ce0ba1a8a0bd99397688780cd92972bab0be85c6
Vikktor93 <Vikktor93@users.noreply.github.com>
2024-05-06 10:46:31 -0400

    Subiendo archivos Python

---
+++ 01-variables.py
@@ -0,0 +1,22 @@
+#Guia Rápida Python - 05 de Mayo de 2024
+#Docente: Victor Saldivia Vera - ICINF ULagos
+
+"""Este es un 
+comentario multiliea"""
+
+#Este es un comentario de una sola linea
+
+#Impresión de Variable (Print Clásico)
+name = "Victor"
+print("Hola soy", name)
+
+#Impresión de Variable (Cadenas Literales - f-string)
+nombre = "Constanza"
+print(f"Hola mi nombres es {nombre}")
+
+#Impresión de Variable (Concatenación)
+nombre2 = "Benjamin"
+print("Hola mi nombre es " + nombre2)
+
+edad = 30
+
