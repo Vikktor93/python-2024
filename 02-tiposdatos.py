63c744314d10ef67e3432dc2f564e4e7ff697da3
Vikktor93 <Vikktor93@users.noreply.github.com>
2024-05-06 12:00:46 -0400

    Se crea nuevo archivo: Tipos de Datos

---
+++ 02-tiposdatos.py
@@ -0,0 +1,7 @@
+"""Guia Rápida: Tipos de Datos"""
+
+# 1. STRINGS
+institucion = "Universidad de Los Lagos"
+asignatura = 'Programación'
+descripcion = """Asignatura de primer semestre
+de la carrera de Ingeniería Civil en Informática"""
