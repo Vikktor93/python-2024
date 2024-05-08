2c51d34dc3547c672ad63859f4e33e974324234a
Vikktor93 <Vikktor93@users.noreply.github.com>
2024-05-08 16:06:42 -0400

    Se agrega funciones de Strings

--- 02-tiposdatos.py
+++ 02-tiposdatos.py
@@ -1,7 +1,23 @@
-"""Guia Rápida: Tipos de Datos"""
+""" GUIA RÁPIDA TIPOS DE DATOS EN PYTHON
+Docente: Victor Saldivia Vera - ICINF ULagos"""
 
 # 1. STRINGS
 institucion = "Universidad de Los Lagos"
 asignatura = 'Programación'
 descripcion = """Asignatura de primer semestre
 de la carrera de Ingeniería Civil en Informática"""
+
+# IMPRIMIR LA POSICIÓN DE UN CARACTER
+print(institucion[-2])
+
+#CONCATENACIÓN
+resultado = print(institucion + " " + asignatura)
+
+# MULTIPLICACIÓN DE TEXTO POR UN NÚMERO
+salida = print(institucion * 4)
+
+# UTILIZANDO LA FUNCIÓN SPLIT EN STRINGS
+print(institucion.split())
+
+print(len(institucion))
+
